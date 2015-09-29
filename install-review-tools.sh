4a8dc09f7c0dd67ac5f59e7253ab83e34aae53ef
Tim Van Steenburgh <tvansteenburgh@gmail.com>
2015-09-29 19:10:16 -0400

    Install next proposed amulet release for testing

--- install-review-tools.sh
+++ install-review-tools.sh
@@ -2,7 +2,11 @@
 set -e
 HOME=/home/ubuntu
 
+# Install next proposed amulet release for testing
+sudo add-apt-repository ppa:juju/proposed -y
 sudo apt-get update -qqy
+sudo apt-get install amulet -qy
+
 sudo apt-get install -qy unzip build-essential charm-tools python-dev python-pip python-virtualenv rsync
 sudo pip install bundletester flake8
 
